c21a89a889b92bf83aaa63759e4f02e909cdbe3c
CalebMerroto-TechConsulting <Caleb.Merroto@techconsulting.tech>
2025-03-26 15:43:28 -0400

    Created XCode project and put together main file structure for project

---
+++ Movie-App-Project/Movie-App-Project/Movie_App_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_App_ProjectApp.swift
+//  Movie-App-Project
+//
+//  Created by Caleb Merroto on 3/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Movie_App_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Movie-App-Project/Movie-App-Project/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movie-App-Project
+//
+//  Created by Caleb Merroto on 3/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
