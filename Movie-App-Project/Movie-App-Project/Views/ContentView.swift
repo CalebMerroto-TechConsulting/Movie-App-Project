ba77be6c3e6f17ed2a590b5d10d7b88f128ece70
CalebMerroto-TechConsulting <Caleb.Merroto@techconsulting.tech>
2025-03-26 16:22:39 -0400

    Added the two main movie list views, (search and favories)
    - FavoritesView: displays all movies user has marked as favorites
    - SearchView: displays a list of movies retrieved from a search and API call
    - ContentView: contains a tab view to switch between search and favorites views

--- Movie-App-Project/Movie-App-Project/Views/ContentView.swift
+++ Movie-App-Project/Movie-App-Project/Views/ContentView.swift
@@ -5,15 +5,20 @@
 //  Created by Caleb Merroto on 3/26/25.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            MovieSearchView()
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+            FavoritesView()
+                .tabItem {
+                    Image(systemName: "heart.fill")
+                }
         }
         .padding()
     }
@@ -21,4 +26,5 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+        .environmentObject(FavoritesManager.shared)
 }
---
+++ Movie-App-Project/Movie-App-Project/Views/FavoritesView.swift
@@ -0,0 +1,34 @@
+//
+//  FavoritesView.swift
+//  Movie-App-Project
+//
+//  Created by Caleb Merroto on 3/26/25.
+//
+
+
+
+import SwiftUI
+
+struct FavoritesView: View {
+    @EnvironmentObject var favorites: FavoritesManager
+    @State var favoritedMovies: [Item] = []
+    
+    var body: some View {
+        VStack {
+            Text("Favorites")
+                .font(.system(size: 32, weight: .bold))
+                .padding()
+            MovieGridView(movies: .constant(favoritedMovies))
+        }
+        .onAppear() {
+            favoritedMovies = favorites.getFavorites().map { movieName in
+                 Item(name: movieName)
+            }
+        }
+    }
+}
+
+#Preview {
+    FavoritesView()
+        .environmentObject(FavoritesManager.shared)
+}
